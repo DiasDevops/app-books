cc0fe3ea20b7345bbe177e5b009b39beec4313be
Breno Amarante <128867553+breno-777@users.noreply.github.com>
2023-09-05 21:45:03 -0300

    Add files via upload

---
+++ src/screens/Favorites/index.jsx
@@ -0,0 +1,13 @@
+import { VStack, HStack, Text } from "native-base";
+
+function Favorites() {
+  return (
+    <VStack>
+      <VStack>
+        <Text>Minha Coleção</Text>
+      </VStack>
+    </VStack>
+  );
+}
+
+export default Favorites;
