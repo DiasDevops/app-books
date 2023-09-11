907865603f75f3a3639a3736c229f9c7f58962df
DiasDevops <diasdevops@gamil.com>
2023-09-11 20:24:52 -0300

    segue home

---
+++ componentes/index.js
@@ -0,0 +1,9 @@
+function Home() {
+    const navigation = useNavigation();
+    const [categories, setCategories] = useState([]);
+    
+  
+    function handleOpenDetails(item) {
+
+    }
+}
---
+++ componentes/style.js
@@ -0,0 +1,10 @@
+import {StyleSheet} from "react-native";
+export default StyleSheet.create({
+    heigth: 20,
+    width:10,
+    border: 10,
+    backgroundColor:"#1e8187",
+    alignItems:"center",
+    margintop:20,
+}
+)
